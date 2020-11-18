5e00537dc5b22badc326987c9649dd20c8659b53
Vladimir Vedernikov <vladimir.vedernikov@ictpro.labra>
2020-11-18 12:33:45 +0200

    update

--- Harjoitus_2.ino
+++ Harjoitus_2.ino
@@ -1,21 +1,21 @@
-#define LED 6
-int val = 0;
+#define LED 5
+#define ON LOW
+#define OFF HIGH
+bool val = digitalRead(5);
 
 void setup() {
   pinMode(LED, OUTPUT);
   Serial.begin(9600);
-  
 }
 
-int viive = 300;
-int LEDViive = 100;
+int viive = 1000;
 
-void loop(){
-  digitalWrite (LED, HIGH);
+void loop(){ 
+  digitalWrite (LED, ON);
+  if (val = true){Serial.println("LED ON");}
   delay(viive);
-  digitalWrite(LED, LOW);
+  digitalWrite(LED, OFF);
+  Serial.println("LED OFF");
   delay(viive);
-  val = digitalRead(LED);
-  digitalWrite(LED, val);
-  Serial.println(val);
+ 
 }
